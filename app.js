5d8904411bc54da0b4680870fedf931bab11d098
Joe Gailey <jgaileyiii@gmail.com>
2020-04-05 16:39:37 -0600

    finished winner functionality, game reset functionality, and included state variables to determine game status.

--- app.js
+++ app.js
@@ -8,24 +8,80 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+init()
 
-
-var scores = [0,0]
-var roundScore = 0
-var activePlayer = 0
-
-
-document.querySelector('.dice').style.display = 'none'
-document.getElementById('score-0').textContent = '0'
-document.getElementById('score-1').textContent = '0'
-document.getElementById('current-0').textContent = '0'
-document.getElementById('current-1').textContent = '0'
+var scores, roundScore, activePlayer,gamePlaying
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
-    var dice = Math.floor(Math.random() * 6) + 1
-    var diceDom = document.querySelector('.dice')
-    diceDom.style.display = 'block'
-    document.querySelector('.dice').style.display = 'block'
-    diceDom.src = 'dice-' + dice + '.png'
+    if(gamePlaying){
+        var dice = Math.floor(Math.random() * 6) + 1
+
+        var diceDom = document.querySelector('.dice')
+        diceDom.style.display = 'block'
+        document.querySelector('.dice').style.display = 'block'
+        diceDom.src = 'dice-' + dice + '.png'
+
+    if (dice !== 1) {
+        //add score
+        roundScore += dice
+        document.querySelector('#current-' + activePlayer).textContent = roundScore
+    } else {
+        nextPlayer()
+        }
+    }    
 })
 
+document.querySelector('.btn-hold').addEventListener('click', function () {
+    //Add Current score to global score.
+    scores[activePlayer] += roundScore
+    //Update UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer]
+    //Check is player won
+    if (scores[activePlayer] >= 100) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!'
+        document.querySelector('.dice').style.display = 'none'
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner')
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active')
+        gamePlaying = false
+    } else {
+        nextPlayer()
+    }
+})
+
+function nextPlayer () {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0
+    roundScore = 0
+
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+
+    document.querySelector('.player-0-panel').classList.toggle('active')
+    document.querySelector('.player-1-panel').classList.toggle('active')
+
+    document.querySelector('.dice').style.display = 'none'
+}
+
+document.querySelector('.btn-new').addEventListener('click', init)
+
+function init () {
+    scores = [0,0]
+    roundScore = 0
+    activePlayer = 0
+    gamePlaying = true
+
+    document.querySelector('.dice').style.display = 'none'
+
+    document.getElementById('score-0').textContent = '0'
+    document.getElementById('score-1').textContent = '0'
+    document.getElementById('current-0').textContent = '0'
+    document.getElementById('current-1').textContent = '0'
+    document.getElementById('name-0').textContent = 'Player 1'
+    document.getElementById('name-1').textContent = 'Player 2'
+    document.querySelector('.player-0-panel').classList.remove('winner')
+    document.querySelector('.player-1-panel').classList.remove('winner')
+    document.querySelector('.player-0-panel').classList.remove('active')
+    document.querySelector('.player-0-panel').classList.add('active')
+    document.querySelector('.player-1-panel').classList.remove('active')
+
+
+}
